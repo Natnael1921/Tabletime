d538974e46f6be3d9a74796f801889a3004fa758
Natnael Mekonnen <your_nnatnaelmekonnen19.com>
2025-07-10 18:17:20 +0300

    Configured and deployed MySQL database to Railway

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use('/api', balanceRoutes);
 
 
 
-const PORT= process.env.PORT;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
 	console.log(` working on port ${PORT}`);
 });
